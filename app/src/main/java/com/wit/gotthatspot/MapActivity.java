ff952c990efba6768f2c0d3fabc45d18cfef822a
Wit Kurowski <witkurowski@gmail.com>
2016-06-05 12:55:19 -0700

    [Fixes PT #120906273] Default to the user's current location.

--- app/src/main/java/com/wit/gotthatspot/MapActivity.java
+++ app/src/main/java/com/wit/gotthatspot/MapActivity.java
@@ -1,54 +1,153 @@
 package com.wit.gotthatspot;
 
-import android.support.v4.app.FragmentActivity;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
-	private GoogleMap googleMap;
+public class MapActivity extends FragmentActivity {
+	private static final float INITIAL_ZOOM = 15.0f;
+	private final State state = new State();
 
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		this.setContentView(R.layout.map);
-
-		// Obtain the SupportMapFragment and get notified when the map is ready to be used.
-		final FragmentManager supportFragmentManager = getSupportFragmentManager();
-		final SupportMapFragment supportMapFragment =
-				(SupportMapFragment) supportFragmentManager
-						.findFragmentById(R.id.map);
-
-		supportMapFragment.getMapAsync(this);
+		this.setupGoogleApiClient();
 	}
 
-	/**
-	 * Manipulates the map once available. This callback is triggered when the map is ready to be
-	 * used. This is where we can add markers or lines, add listeners or move the camera. In this
-	 * case, we just add a marker near Sydney, Australia. If Google Play services is not installed
-	 * on the device, the user will be prompted to install it inside the SupportMapFragment. This
-	 * method will only be triggered once the user has installed Google Play services and returned
-	 * to the app.
-	 */
 	@Override
-	public void onMapReady(final GoogleMap googleMap) {
-		this.googleMap = googleMap;
+	protected void onStart() {
+		super.onStart();
 
-		// Add a marker in Sydney and move the camera
-		final LatLng sydney = new LatLng(-34, 151);
-		final MarkerOptions markerOptions = new MarkerOptions();
+		this.state.googleApiClient.connect();
+	}
 
-		markerOptions.position(sydney);
-		markerOptions.title("Marker in Sydney");
+	@Override
+	protected void onStop() {
+		super.onStop();
 
-		this.googleMap.addMarker(markerOptions);
-		this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+		this.state.googleApiClient.disconnect();
+	}
+
+	private void setupGoogleApiClient() {
+		final GoogleApiClient.Builder googleApiClientBuilder = new GoogleApiClient.Builder(this);
+		final FragmentManager supportFragmentManager = this.getSupportFragmentManager();
+		final ConnectionCallbacks connectionCallbacks = new ConnectionCallbacks(this,
+				supportFragmentManager, this.state);
+
+		googleApiClientBuilder.addConnectionCallbacks(connectionCallbacks);
+
+		final OnConnectionFailedListener onConnectionFailedListener =
+				new OnConnectionFailedListener();
+
+		googleApiClientBuilder.addOnConnectionFailedListener(onConnectionFailedListener);
+		googleApiClientBuilder.addApi(LocationServices.API);
+
+		this.state.googleApiClient = googleApiClientBuilder.build();
+	}
+
+	private static final class ConnectionCallbacks implements GoogleApiClient.ConnectionCallbacks {
+		private final Context context;
+		private final FragmentManager fragmentManager;
+		private final State state;
+
+		private ConnectionCallbacks(final Context context, final FragmentManager fragmentManager,
+				final State state) {
+			this.context = context;
+			this.fragmentManager = fragmentManager;
+			this.state = state;
+		}
+
+		@Override
+		public void onConnected(@Nullable final Bundle bundle) {
+			final SupportMapFragment supportMapFragment =
+					(SupportMapFragment) this.fragmentManager
+							.findFragmentById(R.id.map);
+			final OnMapReadyCallback onMapReadyCallback =
+					new OnMapReadyCallback(this.context, this.state.googleApiClient);
+
+			supportMapFragment.getMapAsync(onMapReadyCallback);
+		}
+
+		@Override
+		public void onConnectionSuspended(final int i) {
+		}
+	}
+
+	private static final class OnConnectionFailedListener
+			implements GoogleApiClient.OnConnectionFailedListener {
+		@Override
+		public void onConnectionFailed(@NonNull final ConnectionResult connectionResult) {
+		}
+	}
+
+	private static final class OnMapReadyCallback implements
+			com.google.android.gms.maps.OnMapReadyCallback {
+		private final Context context;
+		private final GoogleApiClient googleApiClient;
+
+		private OnMapReadyCallback(final Context context, final GoogleApiClient googleApiClient) {
+			this.context = context;
+			this.googleApiClient = googleApiClient;
+		}
+
+		@Override
+		public void onMapReady(final GoogleMap googleMap) {
+			final boolean locationAccessGranted = ActivityCompat
+					.checkSelfPermission(this.context, Manifest.permission.ACCESS_FINE_LOCATION) ==
+					PackageManager.PERMISSION_GRANTED;
+
+			if (locationAccessGranted) {
+				final Location latestLocation =
+						LocationServices.FusedLocationApi.getLastLocation(this.googleApiClient);
+
+				if (latestLocation == null) {
+					final Toast toast =
+							Toast.makeText(this.context,
+									R.string.failed_to_retrieve_latest_location,
+									Toast.LENGTH_SHORT);
+
+					toast.show();
+				} else {
+					final MarkerOptions markerOptions = new MarkerOptions();
+					final double latestLocationLatitude = latestLocation.getLatitude();
+					final double latestLocationLongitude = latestLocation.getLongitude();
+					final LatLng latestLatLng =
+							new LatLng(latestLocationLatitude, latestLocationLongitude);
+
+					markerOptions.position(latestLatLng);
+					googleMap.addMarker(markerOptions);
+
+					final CameraUpdate cameraUpdate =
+							CameraUpdateFactory
+									.newLatLngZoom(latestLatLng, MapActivity.INITIAL_ZOOM);
+
+					googleMap.moveCamera(cameraUpdate);
+				}
+			}
+		}
+	}
+
+	private static final class State {
+		public GoogleApiClient googleApiClient;
 	}
 }
